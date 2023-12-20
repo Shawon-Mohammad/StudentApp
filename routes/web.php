33762a96f94cc735a4208b840da8d8ea0c56761f
Abdul Motaleb Shawon <69819268+Shawon-Mohammad@users.noreply.github.com>
2023-12-20 21:21:04 +0600

    add route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\PermissionController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +21,39 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+
+    Route::get('/index', [StudentController::class, 'index'])->name('index');
+
+    Route::get('/roles', [RoleController::class, 'index'])->name('roles.index');
+    Route::get('/roles/create', [RoleController::class, 'create'])->name('roles.create');
+    Route::post('/roles/store', [RoleController::class, 'store'])->name('roles.store');
+    Route::get('/roles/edit/{id}', [RoleController::class, 'edit'])->name('roles.edit');
+    Route::post('/roles/update/{id}', [RoleController::class, 'update'])->name('roles.update');
+    Route::get('/roles/delete/{id}', [RoleController::class, 'delete'])->name('roles.delete');
+
+    Route::get('/permissions', [PermissionController::class, 'index'])->name('permissions.index');
+    Route::get('/permissions/create', [PermissionController::class, 'create'])->name('permissions.create');
+    Route::post('/permissions/store', [PermissionController::class, 'store'])->name('permissions.store');
+    Route::get('/permissions/edit/{data}', [PermissionController::class, 'edit'])->name('permissions.edit');
+    Route::post('/permissions/update/{data}', [PermissionController::class, 'update'])->name('permissions.update');
+    Route::get('/permissions/delete/{data}', [PermissionController::class, 'delete'])->name('permissions.delete');
+
+    Route::get('/user', [UserController::class, 'index'])->name('user.index');
+    Route::get('/user/create', [UserController::class, 'create'])->name('user.create');
+    Route::post('/user/store', [UserController::class, 'store'])->name('user.store');
+    Route::get('/user/edit/{data}', [UserController::class, 'edit'])->name('user.edit');
+    Route::post('/user/update/{data}', [UserController::class, 'update'])->name('user.update');
+    Route::get('/user/delete/{data}', [UserController::class, 'delete'])->name('user.delete');
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
